57645af96a844938c6ed441cc06f93022ea30a41
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-10-31 15:37:50 -0700

    added script to dynamicly add letter seperators to page

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+    loadComicData()
 });
 
-function comicData() {
+function loadComicData() {
     fetch("https://akabab.github.io/superhero-api/api/all.json")
         .then(res => res.json())
         .then(comicData => {
-            console.log(comicData)
+            createAlphaSeperators(comicData)
         })
 }
 
-comicData()
+function createAlpha(comicArray){
+    const characterLetters = []
+    comicArray.forEach(character =>{
+        if(!characterLetters.includes(character.name[0])){
+            characterLetters.push(character.name[0])
+        }
+    })
+    return characterLetters
+}
+
+function createAlphaSeperators(comicArray){
+    const lettersForSperators = createAlpha(comicArray)
+    const heroCards = document.getElementById("hero-cards")
+    lettersForSperators.map(letter => {
+        const div = document.createElement("div")
+        div.setAttribute("id", letter)
+        const letterHeader = document.createElement("h3")
+        letterHeader.textContent = letter
+        letterHeader.setAttribute("class","letter-header")
+        const sperator = document.createElement("hr")
+        sperator.setAttribute("class","letter-header")
+        div.appendChild(letterHeader)
+        div.appendChild(sperator)
+        heroCards.appendChild(div)
+    })
+    
+}
+
